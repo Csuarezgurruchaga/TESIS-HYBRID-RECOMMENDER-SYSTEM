9daa7888a208f6cf20d729605dd25ad12cbbd2a8
csuarezgurruchaga <csuarezgurruchaga@gmail.com>
2023-12-13 11:50:58 -0300

    Tidying up the data_splitting_utils module

--- source/utils/data_splitting_utils.py
+++ source/utils/data_splitting_utils.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import silhouette_score
-from sklearn.cluster import KMeans
-
+from utils import clusterize_user
 
 """ 
 pros y contras sobre este metodo de split
@@ -69,64 +67,6 @@ def train_test_split(data, userId_col_name="userId", movieId_col_name='movieId',
         train_df.loc[(train_df[userId_col_name] == u) & (train_df[movieId_col_name].isin(items)), rating_col_name] = np.nan
     
     return train_df, y_true, test_items
-
-
-def clusterize_user(u, to_clusterize, max_n_clusters, refine_n_clusters = True):
-    """
-    Clusterizes items for a given user based on ratings using KMeans clustering.
-
-    Parameters:
-        u (int): User ID.
-        to_clusterize (pd.DataFrame): DataFrame containing 'rating' and 'movieId' columns to be clustered.
-        max_n_clusters (int): Maximum number of clusters to consider. A larger number may result in overfitting.
-        refine_n_clusters (bool): If True, refines the number of clusters based on silhouette scores.
-
-    Returns:
-        pd.DataFrame: Clusterized DataFrame with columns 'movieId', 'rating', and 'cluster_label'.
-    """
-    
-    if refine_n_clusters:
-        n_clusters_range = range(2, max_n_clusters+1)
-        # Dictionary to store silhouette scores for each number of clusters
-        sil_scores = {}
-        for n_clusters in n_clusters_range:
-            # Create and fit KMeans model
-            kmeans = KMeans(n_clusters=n_clusters, random_state=1203, n_init = 'auto')
-            cluster_labels = kmeans.fit_predict(np.array(to_clusterize['rating']).reshape(-1, 1))
-            
-            # Calculate Silhouette Score
-            if len(set(cluster_labels))>1:
-                sil_score = silhouette_score(np.array(to_clusterize['rating']).reshape(-1, 1), cluster_labels)
-                sil_scores[n_clusters] = sil_score
-
-                # Find the best number of clusters
-                best_n_clusters = max(sil_scores, key=sil_scores.get)
-        
-        if len(set(cluster_labels))==1:
-            # the user rated similarly all the movies that he saw.
-            best_n_clusters = 1
-            print(f"n_clusters for user {u} is 1, the user rated similarly all the movies that he saw.")
-                
-        # Create and fit KMeans model with the best number of clusters
-        best_kmeans = KMeans(n_clusters=best_n_clusters, random_state=1203, n_init = 'auto')
-        best_cluster_labels = best_kmeans.fit_predict(np.array(to_clusterize['rating']).reshape(-1, 1))
-
-        clusterized_df = pd.DataFrame({'movieId': to_clusterize['movieId'].values,
-                                        'rating': to_clusterize['rating'].values,  
-                                        'cluster_label': best_cluster_labels})
-        return clusterized_df    
-    
-    # if refine_n_clusters is False, the computation cost its reduced and the number of clusters is
-    # set as max_n_clusters
-    best_kmeans = KMeans(n_clusters = max_n_clusters, random_state=1203, n_init = 'auto')
-    best_cluster_labels = best_kmeans.fit_predict(np.array(to_clusterize['rating']).reshape(-1, 1))
-
-    clusterized_df = pd.DataFrame({'movieId': to_clusterize['movieId'].values,
-                                    'rating': to_clusterize['rating'].values,  
-                                    'cluster_label': best_cluster_labels})
-
-    return clusterized_df
-
 
 
 
